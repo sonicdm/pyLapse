949d8b24544529168cb9150ac0e98dee1a53a4dc
sonicdm <sonicdm@gmail.com>
2017-06-10 10:39:22 -0700

    Implemented CronTrigger from apscheduler for filtering images using  cron style syntax

--- pyLapse/ImgSeq/lapsetime.py
+++ pyLapse/ImgSeq/lapsetime.py
@@ -2,6 +2,10 @@
 lapsetime:
 library for handling time operations on image sets.
 """
+import datetime
+
+from tzlocal import get_localzone
+
 import image
 import utils
 import dateutil
@@ -115,3 +119,49 @@ def find_nearest(array, value, fuzzyness=5):
     itemidx, item = min(enumerate(array), key=lambda x: abs(x[1] - value))
     if abs(value - item) <= fuzzyness:
         return item, itemidx
+
+
+def get_fire_times(crontrigger, day):
+    day = datetime.datetime(day.year, day.month, day.day).replace(tzinfo=get_localzone())
+    # print "Day: %s" % day.date()
+    cur_day = day
+    last_fire = day - datetime.timedelta(microseconds=1)
+    times = []
+    seconds = [i for i in range(0, 86399)]
+    while cur_day.date() == day.date():
+        # print "Last Fire: %s" % last_fire
+        now = last_fire + datetime.timedelta(microseconds=1)
+        next_fire = crontrigger.get_next_fire_time(last_fire, now)
+        # print "Next Fire: %s" % next_fire
+        times.append(next_fire.replace(tzinfo=None))
+        cur_day = next_fire
+        last_fire = next_fire
+    return times
+
+
+def cron_image_filter(imageindex, cron_trigger, fuzzy=5):
+    images = []
+    for day, files in sorted(imageindex.iteritems()):
+        dt_day = datetime.datetime.strptime(day, '%Y-%m-%d').replace(tzinfo=get_localzone())
+        next_day = cron_trigger.get_next_fire_time(dt_day, dt_day)
+        fire_times = get_fire_times(cron_trigger, dt_day)
+        if next_day.date() == dt_day.date():
+            # print dt_day
+            last_match = None
+            day_set = {key: value for key, value in sorted(files.iteritems(), key=lambda (k, v): (v, k))}
+            reverse_day_set = {v: k for k, v in day_set.iteritems()}
+            day_timestamps = day_set.values()
+            time_keys = [find_nearest_dt(i, day_timestamps, fuzzy) for i in fire_times]
+            time_keys = filter(lambda x: x != None, time_keys)
+            for key in time_keys:
+                images.append(reverse_day_set[key])
+
+    return images
+
+
+def find_nearest_dt(target_dt, dtlist, fuzzy=5):
+    deltas = [x for x in dtlist if (x - target_dt).seconds <= fuzzy * 60]
+    if len(deltas) < 1:
+        return None
+    else:
+        return min(deltas, key=lambda x: x - target_dt)
